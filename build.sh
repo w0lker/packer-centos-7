d41aad8450fae19b0d3a301cdd77faeada1887b8
w0lker <w0lker.tg@gmail.com>
2016-12-22 17:11:55 +0800

    修改构建脚本

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+temp_files="packer_cache
+output-virtualbox-iso
+/usr/local/etc/ansible/roles/*
+/etc/ansible/roles/*"
+
+case $1 in
+    clean)
+	for temp_file in temp_files;do
+	    rm -rf $temp_file
+	done
+	;;
+    make)
+	ansible-galaxy install -r requirements.txt
+	packer build centos7.json
+	;;
+    install)
+	vagrant box add w0lker/centos7 *-centos7.box
+	;;
+    *)
+	echo "Usage: build.sh [make|install|clean]"
+	;;
+esac
